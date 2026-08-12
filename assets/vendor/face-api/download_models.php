a86fd14c2e45a79c71c8e0f99e90acf7cc1f7230
lesotole <andikawahyuseftiadi1423@gmail.com>
2026-08-12 14:45:07 +0700

    fix: add missing ssd_mobilenetv1_model-shard2 weight file to satisfy tensor shape requirements

--- assets/vendor/face-api/download_models.php
+++ assets/vendor/face-api/download_models.php
@@ -8,6 +8,7 @@ $files = [
     'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/dist/face-api.min.js' => __DIR__ . '/face-api.min.js',
     'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights/ssd_mobilenetv1_model-weights_manifest.json' => $target_dir . 'ssd_mobilenetv1_model-weights_manifest.json',
     'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights/ssd_mobilenetv1_model-shard1' => $target_dir . 'ssd_mobilenetv1_model-shard1',
+    'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights/ssd_mobilenetv1_model-shard2' => $target_dir . 'ssd_mobilenetv1_model-shard2',
     'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights/face_landmark_68_model-weights_manifest.json' => $target_dir . 'face_landmark_68_model-weights_manifest.json',
     'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights/face_landmark_68_model-shard1' => $target_dir . 'face_landmark_68_model-shard1',
     'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights/face_recognition_model-weights_manifest.json' => $target_dir . 'face_recognition_model-weights_manifest.json',
